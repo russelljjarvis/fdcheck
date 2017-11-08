a6b92cf6b0520845f4f83a279765c1947f4a57cb
Russell Jarvis <rjjarvis@asu.edu>
2017-11-08 13:04:05 -0700

    generating report about agreement between GA and grid

--- unit_test/test_exhaustive_search.py
+++ unit_test/test_exhaustive_search.py
@@ -3,7 +3,7 @@
 ##
 NGEN = 3
 MU = 4; NGEN = 3; CXPB = 0.9
-# about 18, models will be made, excluding rheobase search.
+# about 12, models will be made, excluding rheobase search.
 
 ##
 # Grid search parameters:
@@ -33,9 +33,39 @@ lists = pickle.load(open('complete_dump.p','rb'))
 invalid_dtc, pop, logbook, fitnesses = lists[0],lists[1],lists[2], lists[3]
 '''
 dtcpopg = es.run_grid(npoints,nparams)
-from find_min import min_find
+
 # This function searches virtual model data containers to find the values with best scores.
-minimaga,minimagr = min_find(invalid_dtc,dtcpopg)
+def min_find(dtcpop):
+    import numpy as np
+    sovg=[]
+    for i in dtcpop:
+        rt = 0 # running total
+        for values in i.scores.values():
+            rt += sqrt(mean(square(list(values)))
+        sovg.append(rt)
+    dtc = invalid_dtc[np.where(sovg==np.min(sovg))[0][0]]
+    return dtc
+
+def min_max(dtcpop):
+    import numpy as np
+    sovg=[]
+    for i in dtcpop:
+        rt = 0 # running total
+        for values in i.scores.values():
+            rt += sqrt(mean(square(list(values)))
+        sovg.append(rt)
+    dtc = invalid_dtc[np.where(sovg==np.max(sovg))[0][0]]
+    return dtc
+
+minimaga = min_find(invalid_dtc)
+minimagr = min_find(dtcpopg)
+maximagr = min_max(dtcpopg)
+# quantize distance between minimimum error and maximum error.
+quantize_distance = list(np.linspace(minimagr,maximagr,10))
+assert minimaga < quantize_distance[1]
+
+
+
 print('Report: ')
 # This function reports on the deltas brute force obtained versus the GA found attributes.
 for k,v in minimagr.attrs.items():
@@ -49,6 +79,16 @@ print(rmsg)
 print('ga:')
 rmsga=sqrt(mean(square(list(minimaga.scores.values()))))
 print(rmsga)
+print('maximum error:', maximagr)
+
+# Two things to find:
+# How close togethor are the model parameters in parameter space (hyper volume), relative to the dimensions of the HV?
+# ie get the euclidian distance between the two sets of model parameters.
+
+#
+#
+#
+
 exit()
 #quit()
 
